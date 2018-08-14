6837f2acbe5badbd62d33996ff49c01242df7485
sushant124u <sushant124u@gmail.com>
2018-08-14 23:39:19 +0530

    Sushiiii

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal.
+      // Prints "Hello, World" in the terminal windows.
       System.out.println("Hello, World");
    }
 }
